5baaaa977db75b2287ab7a4c1ea316513eae99b7
Franzjodie Domingo <franzjodiedomingo@gmail.com>
2024-01-29 16:22:18 +0800

    final

--- countinasubstring.py
+++ countinasubstring.py
@@ -1,3 +1,8 @@
-#Counting of substring 
+#counting of substring 
 #str_x = "Emma is good developer. Emma is  a writer"
 #string method count ()
+
+string = "Emma is smart. Emma is a good developer. Emma is  a writer"
+sub_string = "Emma"
+
+print(string.count(sub_string))
